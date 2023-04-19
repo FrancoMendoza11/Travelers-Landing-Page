67d588b6f6f546638ee2bf7ee4188d901fcf37ab
Franco <frankomendoza78@gmail.com>
2023-04-18 21:12:22 -0300

    responsive mobile

--- app.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-responsive-btn');
-const nav = document.querySelector('nav');
-
-menuBtn.addEventListener('click', function() {
-  nav.classList.toggle('show');
-});
